b748182936f5031ccec50a44a15936fdcec47aae
Alfie Burdekin <alfieburdekin@icloud.com>
2017-10-18 15:35:20 +0100

    hahaha

--- Lists 1/Form1.cs
+++ Lists 1/Form1.cs
@@ -40,7 +40,19 @@ namespace Lists_1
             richTextBox1.AppendText("\n================\n");
             nums.Remove(3);
             printNums(nums);
-            richTextBox1.AppendText("\n" + average(nums) + "\n");         
+            richTextBox1.AppendText("\n" + average(nums) + "\n");
+            List<int> ints = new List<int>();
+            ints.Add(1);
+            ints.Add(2);
+            ints.Add(3);
+            ints.Add(4);
+            ints.Add(5);
+            ints.Add(6);
+            ints.Add(7);
+            ints.Add(8);
+            ints.Add(9);
+            ints.Add(10);
+            printNums(ints);
         }
     }
 }
